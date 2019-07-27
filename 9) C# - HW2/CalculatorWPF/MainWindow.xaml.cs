dabf00ff626ee01d94a62918e6e2937de03bd023
vandohakim <vandoalhakim.mme29@gmail.com>
2019-07-27 08:08:32 +0700

    Initial commit

--- 9) C# - HW2/CalculatorWPF/MainWindow.xaml.cs
+++ 9) C# - HW2/CalculatorWPF/MainWindow.xaml.cs
@@ -132,10 +132,11 @@ namespace CalculatorWPF
             int value = int.Parse(Display_Result.Text);
             Display_Binary.Text = Convert.ToString(value, 2);
 
+            Display_Postorder.Text = InfixToPostfix(Display.Text);
+
             string revString = ReverseAndCleanString(Display.Text);
             string postFixRevStr = InFixToPostFix(revString);
             string preFixStr = ReverseAndCleanString(postFixRevStr);
-            Display_Postorder.Text = postFixRevStr;
             Display_Preorder.Text = preFixStr;
         }
 
@@ -176,6 +177,77 @@ namespace CalculatorWPF
                 koneksi.Close();
             }
         }
+        //===================================================================================//
+        public static string InfixToPostfix(string exp)
+        {
+            // initializing empty String for result
+            String result = String.Empty;
+
+            // initializing empty stack
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < exp.Length; ++i)
+            {
+                char c = exp[i];
+
+                // If the scanned character is an operand, add it to output.
+                if (Char.IsLetterOrDigit(c))
+                    result += c;
+
+                // If the scanned character is an '(', push it to the stack.
+                else if (c == '(')
+                    stack.Push(c);
+
+                //  If the scanned character is an ')', pop and output from the stack 
+                // until an '(' is encountered.
+                else if (c == ')')
+                {
+                    while (stack.Count != 0 && stack.Peek() != '(')
+                        result += stack.Pop();
+
+                    if (stack.Count != 0 && stack.Peek() != '(')
+                        return "Invalid Expression"; // invalid expression                
+                    else
+                        stack.Pop();
+                }
+                else // an operator is encountered
+                {
+                    while (stack.Count != 0 && Prec(c) <= Prec(stack.Peek()))
+                        result += stack.Pop();
+                    stack.Push(c);
+                }
+
+            }
+
+            // pop all the operators from the stack
+            while (stack.Count != 0)
+                result += stack.Pop();
+
+            return result;
+        }
+
+        // A utility function to return precedence of a given operator
+        // Higher returned value means higher precedence
+        public static int Prec(char ch)
+        {
+            switch (ch)
+            {
+                case '+':
+                case '-':
+                    return 1;
+
+                case '*':
+                case '/':
+                    return 2;
+
+                case '^':
+                    return 3;
+            }
+            return -1;
+        }
+
+        //===================================================================================//
+
         public static void InFixToPreFix(string infixExpression)
         {
 
--- 9) C# - HW2/csharphw2.sql
+++ 9) C# - HW2/csharphw2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 26 Jul 2019 pada 17.27
+-- Waktu pembuatan: 27 Jul 2019 pada 03.04
 -- Versi server: 10.3.16-MariaDB
 -- Versi PHP: 7.3.7
 
@@ -31,8 +31,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `csharphw2` (
   `id` int(11) NOT NULL,
   `display` varchar(255) NOT NULL,
-  `postorder` bigint(11) NOT NULL,
-  `preorder` bigint(11) NOT NULL,
+  `postorder` varchar(255) NOT NULL,
+  `preorder` varchar(255) NOT NULL,
   `decimalresult` bigint(11) NOT NULL,
   `binaryresult` bigint(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -42,11 +42,24 @@ CREATE TABLE `csharphw2` (
 --
 
 INSERT INTO `csharphw2` (`id`, `display`, `postorder`, `preorder`, `decimalresult`, `binaryresult`) VALUES
-(14, '', 0, 0, 10, 1010),
-(15, '', 0, 0, 55, 110111),
-(16, '12+3*6', 0, 0, 30, 11110),
-(18, '255+333', 0, 0, 588, 1001001100),
-(21, '2222', 0, 0, 2222, 100010101110);
+(15, '', '0', '0', 55, 110111),
+(16, '12+3*6', '0', '0', 30, 11110),
+(18, '255+333', '0', '0', 588, 1001001100),
+(21, '2222', '0', '0', 2222, 100010101110),
+(23, '32+32', '0', '0', 64, 1000000),
+(24, '32*6', '0', '0', 192, 11000000),
+(25, '25', '0', '0', 25, 11001),
+(26, '35*6', '0', '0', 210, 11010010),
+(27, '66699/9', '0', '0', 7411, 1110011110011),
+(28, '55+3-9', '0', '0', 49, 110001),
+(29, '12+36/12', '0', '0', 15, 1111),
+(30, '12+36/1', '0', '0', 48, 110000),
+(31, '1', '0', '0', 1, 1),
+(32, '1222', '0', '0', 1222, 10011000110),
+(33, '089', '0', '0', 89, 1011001),
+(34, '2+3*8-7', '238', '0', 19, 10011),
+(35, '2+3*8-6', '238*+6-', '', 20, 10100),
+(37, '5*4+8*7', '54*87*+', '+*54*87', 76, 1001100);
 
 --
 -- Indexes for dumped tables
@@ -66,7 +79,7 @@ ALTER TABLE `csharphw2`
 -- AUTO_INCREMENT untuk tabel `csharphw2`
 --
 ALTER TABLE `csharphw2`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
